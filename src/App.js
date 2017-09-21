6db4ab92438ae0d6848dcc9b49d1ebb450ce436b
tlysecki <tim.lysecki@gmail.com>
2017-09-21 15:01:19 -0400

    Toggles working on click wood type

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import {Link} from 'react-router';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topOrBack: 'none'
+    }
+    this.showWoods = this.showWoods.bind(this)
+  }
+
+  showWoods() {
+    window.location.reload()
+  }
 
   render() {
     return (
@@ -11,8 +22,8 @@ class App extends Component {
           <h1>Tonewood Database</h1>
         </header>
         <nav>
-          <Link to="/woods/top"><button>Tops</button></Link>
-          <Link to="/woods/bns"><button>Backs and Sides</button></Link>
+          <Link to="/woods/top" onClick={this.showWoods}><button>Tops</button></Link>
+          <Link to="/woods/bns" onClick={this.showWoods}><button>Backs and Sides</button></Link>
           {this.props.children}
         </nav>
       </div>
--- src/Wood.js
+++ src/Wood.js
@@ -5,36 +5,85 @@ class WoodList extends Component {
     super();
     this.state = {
       top: false,
-      back: false
+      back: false,
+      woodToShow: '',
+      openInfo: false
     }
+    this.showWood = this.showWood.bind(this)
   }
 
   componentWillMount() {
     const topOrBack = window.location.pathname.split('/')[2]
-    this.setState ({
-      type: topOrBack
+    if (topOrBack === 'top') {
+      this.setState({
+        top: true
+      })
+    } if (topOrBack === 'bns') {
+      this.setState({
+        back: true
+      })
+    }
+  }
+
+  showWood(wood) {
+    this.setState({
+      woodToShow: wood,
+      openInfo: !this.state.openInfo
     })
+
   }
 
   render() {
 
     const woods = this.props.route.woods;
-    const topWood = this.state.top;
-    const backWood = this.state.back;
-    const woodsJSX = woods.map((wood, i) => {
-      return
-      <div className="woodType" style={{ display: ((topWood && woods.top) || (backWood && woods.back)) ? 'block' : 'none' }}>
-        <h3 className="woodName">{wood[i].name}</h3>
-      </div>
-    })
+    const showTop = this.state.top;
+    const showBack = this.state.back;
+    let topWoodsJSX = '';
+    let backWoodsJSX = '';
+    if (showTop) {
+      topWoodsJSX = woods.map((wood, i) => {
+        return <div className="WoodType" key={i} style={{ display: showTop === wood.top ? 'block' : 'none' }}>
+          <ul className="tops">
+            <li className="woodName" onClick={() => this.showWood(wood.name)}>{wood.name}
+              <Specs wood={wood} selected={this.state.woodToShow} openInfo={this.state.openInfo} />
+            </li>
+          </ul>
+        </div>
+      })
+    }
+    if (showBack) {
+      backWoodsJSX = woods.map((wood, i) => {
+        return <div className="WoodType" key={i} style={{ display: showBack === wood.back ? 'block' : 'none' }}>
+          <ul className="backs">
+            <li className="woodName" onClick={() => this.showWood(wood.name)}>{wood.name}
+              <Specs wood={wood} selected={this.state.woodToShow} openInfo={this.state.openInfo} />
+            </li>
+          </ul>
+        </div>
+      })
+    }
 
     return (
       <div className="woodList">
-        {woodsJSX}
-        <p>and what about now?</p>
+        {topWoodsJSX}
+        {backWoodsJSX}
       </div>
     );
   }
 }
 
 export default WoodList;
+
+class Specs extends Component {
+  render() {
+    return (
+      <div style={{ display: this.props.wood.name === this.props.selected ? 'block' : 'none' }}>
+        <ul className="woodDetails" style={{ display: this.props.openInfo ? 'block' : 'none'}}>
+          <li>Species: {this.props.wood.species}</li>
+          <li>Janka hardness: {this.props.wood.janka}</li>
+          <li>{this.props.wood.description}</li>
+        </ul>
+      </div>
+    )
+  }
+}
